1519ebe918e2d1aea3a028ad5a721ec44f9a65d2
Aman028 <aman_b210964mt@nitc.ac.in>
2025-05-14 01:59:38 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,21 +2,72 @@ const express = require("express"); //referencing to folder of express in node_m
 
 const app = express();
 
+//?->it means b is not compulsory
+// app.get(/\/ab?c/, (req, res) => {
+//   res.send("Matched /abc or /ac");
+// });
 
-//it takes all the request
-app.use("/",(req,res)=>{
-    res.send("hii from dashboard");
+
+//+-> it means addition sign means we can add as many character we want
+// app.get(/\/a(b)+c/, (req, res) => {
+//   res.send("ultrimate routing+");
+// });
+
+// app.get(/\/a(bc)+c/, (req, res) => {
+//   res.send("ultrimate routing+");
+// });
+
+//*-> it means we can put anything in between
+// app.get(/\/a.*bc/,(req,res)=>{
+//   res.send("ultimate routing*");
+// })
+
+// if we will write in this two line // if it is present in route it will work
+// app.get(/a/,(req,res)=>{
+//   res.send("regex routing");
+// })
+
+// req /user /user/xyz  /user/1 you can do dynamic routing using /user/:id
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+
+app.get("/user/:userid", (req, res) => {
+  //jo semicolon k baad h wahi likhenge params k baad
+  const userId = req.params.userid;
+  res.send(`Requested user with id: ${userId}`);
 });
 
 
+app.post("/user", (req, res) => {
+  res.send("data succewssfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("data deleted succesfully");
+});
+
+//app.use take all methods-- put, post, get, delete.......
+
+app.use("/hello/2", (req, res) => {
+  res.send("hello 2");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("hello hello");
+});
+
 app.use("/test", (req, res) => {
-    res.send("hello from test server");
+  res.send("hello from test server");
+});
+//it takes all the request
+app.use("/", (req, res) => {
+  res.send("hii from dashboard");
 });
 
 app.use((req, res) => {
   res.send("Hello from the server");
 });
-
 
 app.listen(3000, () => {
   console.log("server successfully listening on port 3000....");
